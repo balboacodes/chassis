bc7c6be066fb6b819820a6d75a4a9cd65f38e771
Balboa <20909423+balboacodes@users.noreply.github.com>
2025-11-09 19:40:53 -0500

    wip

--- app/http/controllers/HomeController.ts
+++ app/http/controllers/HomeController.ts
@@ -1,7 +1,12 @@
 import { type Request, type Response } from 'express';
+import Config from '../../../src/framework/Config.js';
+import { inject } from '../../../src/framework/support/decorators/inject.js';
 
+@inject([Config])
 export default class HomeController {
+    public constructor(private config: Config) {}
+
     public index(_req: Request, res: Response): void {
-        res.send('hello');
+        res.send(this.config.get('app.name'));
     }
 }
