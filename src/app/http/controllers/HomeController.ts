a205064b88ee99d9e98e52a3d6f670ade12499b1
Balboa <20909423+balboacodes@users.noreply.github.com>
2025-11-12 16:24:08 -0500

    wip

--- src/app/http/controllers/HomeController.ts
+++ src/app/http/controllers/HomeController.ts
@@ -1,17 +1,11 @@
 import type { Request, Response } from 'express';
-import { Config, inject } from '../../../framework/index.ts';
+import { Config } from '../../../framework/index.ts';
 
-@inject
 export default class HomeController {
-    // @ts-ignore
-    public constructor(private config: Config) {}
-
     /**
      * Display a listing of the resource.
      */
-    @inject
-    public index(_req: Request, res: Response, _config: Config) {
-        // console.log(config);
-        res.send('done');
+    public index(_req: Request, res: Response) {
+        res.send(Config.get('app'));
     }
 }
--- src/framework/index.ts
+++ src/framework/index.ts
@@ -1,6 +1,6 @@
 export { default as App } from './App.ts';
-export { default as Config } from './Config.ts';
 export { default as ServiceProvider } from './providers/ServiceProvider.ts';
 export { default as inject } from './support/decorators/inject.ts';
+export { default as Config } from './support/facades/Config.ts';
 export { default as Route } from './support/facades/Route.ts';
 export { app, config, route } from './support/helpers.ts';
