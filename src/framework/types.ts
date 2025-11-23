457680fef457754dbe6a182b7c3b86d4313329ae
Balboa <20909423+balboacodes@users.noreply.github.com>
2025-11-23 12:18:10 -0500

    wip

--- src/framework/types.ts
+++
@@ -1,6 +0,0 @@
-export type Abstract = string | symbol | Class;
-
-export type Class<TInstance = unknown> = {
-    // deno-lint-ignore no-explicit-any
-    new (...args: any[]): TInstance;
-} & object;
